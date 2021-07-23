906dda0e5cc5f97a6708ea7222fa7d36b714798d
Hitesh <hhbhor@gmail.com>
2021-07-23 23:11:38 +0530

    Signing In The User

--- app/src/main/java/com/gts/themanager/activities/SignInActivity.kt
+++ app/src/main/java/com/gts/themanager/activities/SignInActivity.kt
@@ -1,15 +1,28 @@
 package com.gts.themanager.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import android.view.WindowManager
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.gts.themanager.R
 import kotlinx.android.synthetic.main.activity_sign_in.*
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
-class SignInActivity : AppCompatActivity() {
+class SignInActivity : BaseActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        auth = Firebase.auth
 
         setupActionBar()
 
@@ -17,6 +30,11 @@ class SignInActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
+
+        btn_sign_in.setOnClickListener{
+
+            signInRegisteredUser()
+        }
     }
 
     private fun setupActionBar(){
@@ -29,5 +47,56 @@ class SignInActivity : AppCompatActivity() {
         }
 
         toolbar_sign_in_activity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    private fun validateForm(email:String, password:String): Boolean{
+
+        return when{
+            TextUtils.isEmpty(email) ->{
+                showErrorSnackBar("Please enter email address")
+                false
+            }
+
+            TextUtils.isEmpty(password) ->{
+                showErrorSnackBar("Please enter password")
+                false
+            }
+            else -> {
+                true
+            }
+        }
+    }
+
+    private fun signInRegisteredUser(){
+
+        var email: String= et_email_sign_in.text.toString().trim{it <= ' '}
+        var password: String= et_password_sign_in.text.toString().trim{it <= ' '}
+
+        if (validateForm(email, password)) {
+
+            showProgressDialog(resources.getString(R.string.please_wait))
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+
+                        hideProgressDialog()
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("Sign In", "signInWithEmail:success")
+                        val user = auth.currentUser
+                        startActivity(Intent(this, MainActivity::class.java))
+                        finish()
+
+                    } else {
+
+                        hideProgressDialog()
+                        // If sign in fails, display a message to the user.
+                        Log.w("Sign In", "signInWithEmail:failure", task.exception)
+                        Toast.makeText(baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+
+                    }
+                }
+        }
     }
 }
