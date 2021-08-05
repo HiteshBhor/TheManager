6824bfada360ff8193a428bd5bee45e8f2f8fea2
Hitesh <hhbhor@gmail.com>
2021-08-05 22:58:27 +0530

    The TaskList Activity.

---
+++ app/src/main/java/com/gts/themanager/activities/TaskListActivity.kt
@@ -0,0 +1,12 @@
+package com.gts.themanager.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gts.themanager.R
+
+class TaskListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task_list)
+    }
+}
